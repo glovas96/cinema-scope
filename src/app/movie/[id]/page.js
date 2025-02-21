d223388fa9eb63bbab6b61c6f63d1947f5a51bdf
glovas96 <alekceygolovko@gmail.com>
2025-02-21 16:05:11 +0300

    feat(movie): add HTTP response check

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,19 +1,32 @@
-import MovieDetails from './MovieDetails';
+import MovieDetails from '../../components/MovieDetails';
 
 export default async function MoviePage({ params }) {
     const { id } = await params;
 
     // Fetch movie data on the server
-    const res = await fetch(
-        `${process.env.OMDB_API_URL}/?apikey=${process.env.OMDB_API_KEY}&i=${id}`,
-        { cache: 'no-store' }
-    );
-    const movie = await res.json();
+    try {
+        const res = await fetch(
+            `${process.env.OMDB_API_URL}/?apikey=${process.env.OMDB_API_KEY}&i=${id}`,
+            { cache: 'no-store' }
+        );
 
-    return (
-        <main>
-            {/* Client component renders interactive UI */}
-            <MovieDetails movie={movie} />
-        </main>
-    );
+        // check HTTP response
+        if (!res.ok) throw new Error;
+
+        const movie = await res.json();
+
+        return (
+            <main>
+                {/* Client component renders interactive UI */}
+                <MovieDetails movie={movie} />
+            </main>
+        );
+
+    } catch (error) {
+        // fallback UI when error
+        return <p>Error loading movie details 😢</p>;
+    }
 }
+
+
+
